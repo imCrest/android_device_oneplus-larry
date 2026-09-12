63f90868d480256269b46e7724951e2d18cba416
C R E S T <217463890+imCrest@users.noreply.github.com>
2026-09-12 19:30:56 +0530

    Update and rename lineage_larry.mk to infinity_larry.mk

--- infinity_larry.mk
+++ infinity_larry.mk
@@ -15,8 +15,22 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from larry device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+INFINITY_MAINTAINER := SUJΛL
+INFINITY_BUILD_TYPE := UNOFFICIAL
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+INFINITY_OTA_URL := https://raw.githubusercontent.com/imCrest/Infinityx-Release/main/larry.json
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.product.marketname=OnePlus Nord CE 3 Lite \
+    ro.infinity.soc=Snapdragon 695 5G \
+    ro.infinity.battery=5000 mAh \
+    ro.infinity.display=1080 x 2400, 120 Hz \
+    ro.infinity.camera=108MP + 2MP + 2MP + 16MP
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_larry
+PRODUCT_NAME := infinity_larry
 PRODUCT_DEVICE := larry
 PRODUCT_BRAND := oneplus
 PRODUCT_MODEL := CPH2467
